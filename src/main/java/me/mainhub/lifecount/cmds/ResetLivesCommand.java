a4f7d2ef58bd6db6844056799656b14de5ba9e6f
Dumbstuff <61812799+DumbstuffTC@users.noreply.github.com>
2021-08-09 17:58:25 -0400

    make classes and packages (p1)
    title

---
+++ src/main/java/me/mainhub/lifecount/cmds/ResetLivesCommand.java
@@ -0,0 +1,12 @@
+package me.mainhub.lifecount.cmds;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ResetLivesCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
