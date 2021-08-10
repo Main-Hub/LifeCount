28f85fedb24109f384ddc25c76bf8d9597a26b55
Dumbstuff <61812799+DumbstuffTC@users.noreply.github.com>
2021-08-10 13:54:52 -0400

    Update DeathEvent.java

--- src/main/java/me/mainhub/lifecount/events/DeathEvent.java
+++ src/main/java/me/mainhub/lifecount/events/DeathEvent.java
@@ -6,10 +6,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.io.File;
+
 public class DeathEvent implements Listener {
     public void onDeath(PlayerDeathEvent e) {
         Player p = e.getEntity();
         Player p2 = p.getKiller();
+        private File PlayerDeathsFile = new File("plugins/LifeCount", p.getUniqueId() + ".yml")
         if (p2 instanceof Entity) {
             e.setDeathMessage(ChatColor.RED + p.getDisplayName() + " was brutally murdered by " + p2.getDisplayName());
 
