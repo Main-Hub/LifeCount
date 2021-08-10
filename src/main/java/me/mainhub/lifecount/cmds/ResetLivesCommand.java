3d3e5f798ffe2b6ab42247e828e476ce73a3a6ce
Dumbstuff <61812799+DumbstuffTC@users.noreply.github.com>
2021-08-10 13:44:27 -0400

    Add DeathEvent
    Add DeathEvent to log deaths.

--- src/main/java/me/mainhub/lifecount/cmds/ResetLivesCommand.java
+++ src/main/java/me/mainhub/lifecount/cmds/ResetLivesCommand.java
@@ -1,12 +1,16 @@
 package me.mainhub.lifecount.cmds;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ResetLivesCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player target = Bukkit.getPlayerExact(args[0]);
+        
         return false;
     }
 }
---
+++ src/main/java/me/mainhub/lifecount/events/DeathEvent.java
@@ -0,0 +1,10 @@
+package me.mainhub.lifecount.events;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathEvent implements Listener {
+    public void onDeath(PlayerDeathEvent e) {
+
+    }
+}
